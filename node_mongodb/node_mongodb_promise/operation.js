41f7eaa435f4df487f5ac0e02d0fcac3120c028e
gourabb68 <gourabb68@gmail.com>
2020-04-02 23:57:23 +0530

    node with MongoDB Promises

---
+++ node_mongodb/node_mongodb_promise/operation.js
@@ -0,0 +1,24 @@
+//This is the file based node module where we have
+//all generic CRUD operation 
+const assert = require('assert');
+exports.insertDocument = (db,document,collection,callback)=>{
+   const coll = db.collection(collection);
+   return coll.insert(document);
+   
+
+};
+
+exports.findDocuments = (db, collection, callback) => {
+    const coll = db.collection(collection);
+    return coll.find({}).toArray();
+};
+
+exports.removeDocument = (db, document, collection, callback) => {
+    const coll = db.collection(collection);
+    return coll.deleteOne(document);
+};
+
+exports.updateDocument = (db, document, update, collection, callback) => {
+    const coll = db.collection(collection);
+    return coll.updateOne(document, { $set: update }, null);
+};
