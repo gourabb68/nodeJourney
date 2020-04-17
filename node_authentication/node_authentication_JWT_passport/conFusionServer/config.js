3986fbd2b8acbd2b70326df6538f9bba39265c8f
gourabb68 <gourabb68@gmail.com>
2020-04-18 02:32:53 +0530

    node with jwt token with passport

---
+++ node_authentication/node_authentication_JWT_passport/conFusionServer/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+ "secretKey": '12345-54321-98745-45612',
+ "mongoUrl": 'mongodb://localhost:27017/conFusion'
+}
---
+++ node_authentication/node_authentication_JWT_passport/conFusionServer/models/user.js
@@ -0,0 +1,34 @@
+//--------with passport-local-mongoose
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
+
+var User = new Schema({
+    admin:   {
+        type: Boolean,
+        default: false
+    }
+});
+User.plugin(passportLocalMongoose);
+
+//--------without passport-local-mongoose
+// var mongoose = require('mongoose');
+// var Schema = mongoose.Schema;
+
+// var User = new Schema({
+//     username: {
+//         type: String,
+//         required: true,
+//         unique: true
+//     },
+//     password:  {
+//         type: String,
+//         required: true
+//     },
+//     admin:   {
+//         type: Boolean,
+//         default: false
+//     }
+// });
+
+module.exports = mongoose.model('User', User);
