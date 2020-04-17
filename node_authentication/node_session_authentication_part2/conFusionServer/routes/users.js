ff3a3bc605a037a176a73fa3afcdfa51cc73f0e9
gourabb68 <46346614+gourabb68@users.noreply.github.com>
2020-04-17 17:04:56 +0530

    changed

--- node_authentication/node_session_authentication_part2/conFusionServer/routes/users.js
+++ node_authentication/node_session_authentication_part2/conFusionServer/routes/users.js
@@ -51,7 +51,7 @@ router.post('/login', (req, res, next) => {
     .then((user) => {
       if (user === null) {
         var err = new Error('User ' + username + ' does not exist!');
-        err.status = 403;
+        err.statusCode = 403;
         return next(err);
       }
       else if (user.password !== password) {
