e34b6ca983424dbed2e1f0feca466d9cf5e70003
gourabb68 <gourabb68@gmail.com>
2020-05-08 02:38:38 +0530

    node with assignment2

---
+++ Assignment2/conFusionServer/model/dishes.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+
+var commentSchema = new Schema({
+    rating:  {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    comment:  {
+        type: String,
+        required: true
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+}, {
+    timestamps: true
+});
+const dishSchema = new Schema({
+    name :{
+        type: String,
+        required: true,
+        unique: true //no 2 document have the same field value name
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    label: {
+        type: String,
+        default: ''
+    },
+    price: {
+        type: Currency,
+        require: true,
+        min: 0
+    },
+    featured: {
+        type: Boolean,
+        default: false
+    },
+    comments: [commentSchema]
+},{
+ timestamps: true
+
+});
+
+
+var Dishes = mongoose.model('Dish',dishSchema);
+module.exports = Dishes;
+
+/* if you are getting path validation error then comment the required true part
+*/
